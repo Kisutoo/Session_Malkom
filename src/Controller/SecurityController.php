cf2c5dcdbda8423e16a4aa1cc16bf3ed25f8d65e
kisutoo <malk-om@outlook.fr>
2025-07-15 11:59:20 +0200

    mise en place d'un rate limiter

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,20 +4,29 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\RateLimiter\RateLimiterFactoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, RateLimiterFactoryInterface $anonymousApiLimiter, Request $request): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
+        // $limiter = $anonymousApiLimiter->create($request->getClientIp());
+
+        // if (false === $limiter->consume(1)->isAccepted()) {
+        //     throw new TooManyRequestsHttpException();
+        // }
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
