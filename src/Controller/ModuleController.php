a9f260562c042ee761d1c1d2f02df8c3509a124b
kisutoo <malk-om@outlook.fr>
2025-07-15 09:47:14 +0200

    test

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -15,6 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
 class ModuleController extends AbstractController
 {
 
